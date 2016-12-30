ec8658947cf5039e81e805633a0dafa73fa23e5a
Dinara Aleskarova <dinara.aleskarova@simbirsoft.com>
2016-12-30 14:17:51 +0400

    part 7 - task 1

---
+++ twisted-deferred/defer-9_task1.py
@@ -0,0 +1,31 @@
+import sys
+
+from twisted.internet.defer import Deferred
+
+
+def got_poem(poem):
+    print poem
+    return poem     # without it poem_done gets None as an argument
+
+
+def poem_failed(err):
+    print >>sys.stderr, 'poem download failed'
+    print >>sys.stderr, 'I am terribly sorry'
+    print >>sys.stderr, 'try again later?'
+
+
+def poem_done(_):
+    print _
+    from twisted.internet import reactor
+    reactor.stop()
+
+d = Deferred()
+
+d.addCallbacks(got_poem, poem_failed)
+d.addBoth(poem_done)
+
+from twisted.internet import reactor
+
+reactor.callWhenRunning(d.callback, 'Another short poem.')
+
+reactor.run()
